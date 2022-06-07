a4c2033bd2dc6aa43c5b386173ff26055fe8c079
Thomas Viana <thomasviana16@gmail.com>
2022-06-07 17:18:08 -0500

    Define App structure.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,15 @@
+import { Footer, Navbar, Services, Welcome } from "./components";
 
 const App =() => {
 
   return (
-    <div className="App">
-      <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+    <div className="min-h-screen">
+      <div className="gradient-bg-welcome">
+        <Navbar />
+        <Welcome />
+      </div>
+      <Services />
+      <Footer />
     </div>
   )
 }
