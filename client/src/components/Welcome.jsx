005abf2b2786fc355ed3346a8c4f9fbb75a90312
Thomas Viana <thomasviana16@gmail.com>
2022-06-09 17:55:45 -0500

    Update welcome component to make transactions.

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -4,6 +4,7 @@ import { BsInfoCircle } from "react-icons/bs";
 import { SiEthereum } from "react-icons/si";
 import { Loader } from "../components";
 import { TransactionContext } from "../context/TransactionContext";
+import { shortenAddress } from "../utils/shortenAddress";
 
 const companyCommonStyles = "min-h-[70px] sm:px-0 px-2 sm:min-w-[120px] flex justify-center items-center border-[0.5px] border-gray-400 text-sm font-light text-white";
 
@@ -19,19 +20,15 @@ const Input = ({ placeholder, name, type, value, handleChange }) => (
 );
 
 const Welcome = () => {
-  const { connectWallet, currentAccount } = useContext(TransactionContext);
+  const { connectWallet, currentAccount, formData, handleChange, sendTransaction } = useContext(TransactionContext);
 
-  // const handleSubmit = (e) => {
-  //   const { addressTo, amount, keyword, message } = formData;
+  const handleSubmit = (event) => {
+    const { addressTo, amount, keyword, message } = formData;
+    event.preventDefault();
+    if (!addressTo || !amount || !keyword || !message) return;
+    sendTransaction();
+  };
 
-  //   e.preventDefault();
-
-  //   if (!addressTo || !amount || !keyword || !message) return;
-
-  //   sendTransaction();
-  // };
-
-  const handleSubmit = () => { };
 
   return (
     <div className="flex w-full justify-center items-center">
@@ -85,8 +82,7 @@ const Welcome = () => {
               </div>
               <div>
                 <p className="text-white font-light text-sm">
-                  Address
-                  {/* {shortenAddress(currentAccount)} */}
+                  {shortenAddress(currentAccount)}
                 </p>
                 <p className="text-white font-semibold text-lg mt-1">
                   Ethereum
@@ -95,10 +91,10 @@ const Welcome = () => {
             </div>
           </div>
           <div className="p-5 sm:w-96 w-full flex flex-col justify-start items-center blue-glassmorphism">
-            <Input placeholder="Address To" name="addressTo" type="text" handleChange={() => { }} />
-            <Input placeholder="Amount (ETH)" name="amount" type="number" handleChange={() => { }} />
-            <Input placeholder="Keyword (Gif)" name="keyword" type="text" handleChange={() => { }} />
-            <Input placeholder="Enter Message" name="message" type="text" handleChange={() => { }} />
+            <Input placeholder="Address To" name="addressTo" type="text" handleChange={handleChange} />
+            <Input placeholder="Amount (ETH)" name="amount" type="number" handleChange={handleChange} />
+            <Input placeholder="Keyword (Gif)" name="keyword" type="text" handleChange={handleChange} />
+            <Input placeholder="Enter Message" name="message" type="text" handleChange={handleChange} />
 
             <div className="h-[1px] w-full bg-gray-400 my-2" />
 
