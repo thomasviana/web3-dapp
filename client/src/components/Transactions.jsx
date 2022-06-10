58e0cf52183f0613f421ad1d43f0c5536eb8eef5
Thomas Viana <thomasviana16@gmail.com>
2022-06-10 09:10:53 -0500

    Show real transactions.

--- client/src/components/Transactions.jsx
+++ client/src/components/Transactions.jsx
@@ -1,7 +1,6 @@
-import React, { useContext } from "react";
+import { useContext } from "react";
 import { TransactionContext } from "../context/TransactionContext";
 import useFetch from "../hooks/useFetch";
-import dummyData from "../utils/dummyData";
 import { shortenAddress } from "../utils/shortenAddress";
 
 const TransactionsCard = ({ addressTo, addressFrom, timestamp, message, keyword, amount, url }) => {
@@ -46,7 +45,7 @@ const TransactionsCard = ({ addressTo, addressFrom, timestamp, message, keyword,
 };
 
 const Transactions = () => {
-  const { transactions, currentAccount } = useContext(TransactionContext);
+  const { transactions, currentAccount, isLoading } = useContext(TransactionContext);
 
   return (
     <div className="flex w-full justify-center items-center 2xl:px-20 gradient-bg-transactions">
@@ -62,7 +61,7 @@ const Transactions = () => {
         )}
 
         <div className="flex flex-wrap justify-center items-center mt-10">
-          {dummyData.reverse().map((transaction, i) => (
+          {transactions.reverse().map((transaction, i) => (
             <TransactionsCard key={i} {...transaction} />
           ))}
         </div>
